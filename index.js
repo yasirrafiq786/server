cca5fc34842374780c22ea1786fd0cd40b73b299
Yasir Malik <yasirrafiq786@gmail.com>
2020-09-29 21:34:19 +1000

    Added cookie-session

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI, {
