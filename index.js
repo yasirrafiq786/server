e39ebf10fb6e778008cc9525b44c351118081d27
Yasir Malik <yasirrafiq786@gmail.com>
2020-09-29 19:45:04 +1000

    removed deprecation

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
 
