4a6a281c03f754bedcbafa88d8ba232ae2cac957
Yasir Malik <yasirrafiq786@gmail.com>
2020-10-01 18:54:38 +1000

    finished making a basic auth reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
