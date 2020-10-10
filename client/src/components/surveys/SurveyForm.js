623e24a0087892699edde31038933d166e39b20b
Yasir Malik <yasirrafiq786@gmail.com>
2020-10-10 13:54:29 +1100

    added lodash

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form';
+import SurveyField from './SurveyField';
+import _ from 'lodash';
+
+const FIELDS = [
+  {label: 'Survey Title', name: 'title'},
+  {label: 'Subject Line', name: 'subject'},
+  {label: 'Email Body', name: 'body'},
+  {label: 'Recipients List', name: 'emails'},
+];
 
 class SurveyForm extends Component {
+  renderField() {
+    FIELDS.map;
+  }
+
   render() {
     return (
       <div>
         <form
           onSubmit={this.props.handleSubmit((values) => console.log(values))}
         >
-          <Field type="text" name="surveyTitle" component="input" />
+          {this.renderField()}
           <button type="submit">Submit</button>
         </form>
       </div>
