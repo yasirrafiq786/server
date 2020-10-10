95b847e53a42bf7e6f7d02297373ab6696eaef15
Yasir Malik <yasirrafiq786@gmail.com>
2020-10-10 13:04:23 +1100

    added redux form

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import SurveyForm from './SurveyForm';
+
+class SurveyNew extends Component {
+  render() {
+    return <SurveyForm />;
+  }
+}
+
+export default SurveyNew;
