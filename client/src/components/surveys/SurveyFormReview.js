89199fd13b6bf9c7a3e59dfdc859331a85d145e3
Yasir Malik <yasirrafiq786@gmail.com>
2020-10-11 21:33:56 +1100

    moving on to webhooks

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import {connect} from 'react-redux';
 import formFields from './formFields';
 import _ from 'lodash';
 import * as actions from '../../actions/index';
+import {withRouter} from 'react-router-dom';
 
-const SurveyFormReview = ({onCancel, formValues, submitSurvey}) => {
+const SurveyFormReview = ({onCancel, formValues, submitSurvey, history}) => {
   const reviewFields = _.map(formFields, (field) => {
     return (
       <div key={field.label}>
@@ -26,7 +27,7 @@ const SurveyFormReview = ({onCancel, formValues, submitSurvey}) => {
       </button>
       <button
         className="green btn-flat white-text right"
-        onClick={() => submitSurvey(formValues)}
+        onClick={() => submitSurvey(formValues, history)}
       >
         Send Survey
         <i className="material-icons right">email</i>
@@ -39,4 +40,4 @@ function mapStateToProps(state) {
   return {formValues: state.form.surveyForm.values};
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
